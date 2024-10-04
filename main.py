bdfa74f7dcde81e36974f1354417a12c2320484a
PandieYay <babyedison@gmail.com>
2024-10-04 23:20:55 +0800

    Working gradient descent

--- main.py
+++ main.py
@@ -2,46 +2,56 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-def gradient_descent(x, y, iterations=1000, learning_rate=0.0001):
+# Gradient is weight, bias is intercept
+def gradient_descent(x, y, iterations=1000, learning_rate=0.000001, stopping_threshold = 1e-6):
 	intercept = 0.01
 	gradient = 0.1
 	m = len(x)
 
+	costs = []
 	gradients = []
+	previous_cost = None
 
 	def estimatePrice(mileage):
 		return intercept + (gradient * mileage)
 
 	def MSE():
-		# TODO
-		equation = (1 / m) * (estimatePrice(x[1]) - y[1]) ** 2
+		summationMSE = 0
+		for count in range(m):
+			summationMSE += (estimatePrice(x[count]) - y[count])**2
+		cost = (1 / m) * summationMSE
+		return cost
 
 	for i in range(iterations):
 		summationIntercept = 0
 		summationGradient = 0
 
-		current_cost = MSE()
-		gradients.append(gradient)
 		# m will already be - 1 as per formula
 		for count in range(m):
 			summationIntercept += estimatePrice(x[count]) - y[count]
 			summationGradient += (estimatePrice(x[count]) - y[count]) * x[count]
+		current_cost = MSE()
 
-		intercept = learning_rate * (1 / m) * summationIntercept
-		gradient = learning_rate * (1 / m) * summationGradient
+		if previous_cost and abs(previous_cost-current_cost)<=stopping_threshold:
+			break
+		previous_cost = current_cost
+		costs.append(current_cost)
+		gradients.append(gradient)
+		intercept -= learning_rate * (1 / m) * summationIntercept
+		gradient -= learning_rate * (1 / m) * summationGradient
 		# Printing the parameters for each 1000th iteration
 		print(
-			f"Iteration {i+1}: Gradient \
+			f"Iteration {i+1}: Cost {current_cost}, Gradient \
 		{gradient}, Intercept {intercept}"
 		)
-	# plt.figure(figsize = (8,6))
-	# plt.plot(weights, costs)
-	# plt.scatter(weights, costs, marker='o', color='red')
-	# plt.title("Cost vs Weights")
-	# plt.ylabel("Cost")
-	# plt.xlabel("Weight")
-	# plt.show()
+	plt.figure(figsize = (8,6))
+	plt.plot(gradients, costs)
+	plt.scatter(gradients, costs, marker='o', color='red')
+	plt.title("Cost vs Weights")
+	plt.ylabel("Cost")
+	plt.xlabel("Weight")
+	plt.show()
+	return gradient, intercept
 
 
 def main():
@@ -56,31 +66,21 @@ def main():
 	# Convert km to miles
 	milesArray = myData.km / 1.609
 
-	gradient_descent(milesArray, myData.price)
+	X = np.array([50, 100, 200])
+	Y = np.array([50, 100, 200])
 
-	# # y=mx+b
-	# theta0 = 0
-	# theta1 = 0
-	# learningRate = 0.1
+	estimated_weight, estimated_bias = gradient_descent(X, Y)
+	print(f"Estimated Weight: {estimated_weight}\nEstimated Bias: {estimated_bias}")
 
-	# for index in range(10):
-	#	 summationTmp0 = 0
-	#	 summationTmp1 = 0
-	#	 # m will already be - 1 as per formula
-	#	 for i in range(m):
-	# summationTmp0 += estimatePrice(myData.km[i] / 1.609) - myData.price[i]
-	#		 summationTmp1 += (estimatePrice(myData.km[i] / 1.609) - myData.price[i]) * (myData.km[i] / 1.609)
-	#	 print("YIPPEE", summationTmp0)
-	#	 print("YIPPEE", summationTmp1)
-	#	 tmpTheta0 = 0
-	#	 tmpTheta1 = 0
-	#	 tmpTheta0 = learningRate * (1 / len(myData.km)) * summationTmp0
-	#	 tmpTheta1 = learningRate * (1 / len(myData.km)) * summationTmp1
-	#	 theta0 = tmpTheta0
-	#	 theta1 = tmpTheta1
-	#	 print(theta0)
-	#	 print(theta1)
+	Y_pred = estimated_weight*X + estimated_bias
 
-
+	# Plotting the regression line
+	plt.figure(figsize = (8,6))
+	plt.scatter(X, Y, marker='o', color='red')
+	plt.plot([min(X), max(X)], [min(Y_pred), max(Y_pred)], color='blue',markerfacecolor='red',
+			markersize=10,linestyle='dashed')
+	plt.xlabel("X")
+	plt.ylabel("Y")
+	plt.show()
 if __name__ == "__main__":
 	main()
